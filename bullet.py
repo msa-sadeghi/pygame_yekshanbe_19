e7c79c220bba193b7defd05156b0cd89b43981fd
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-04-20 19:35:17 +0330

    gg

--- bullet.py
+++ bullet.py
@@ -4,9 +4,22 @@ class Bullet(Sprite):
     def __init__(self, x, y, direction):
         super().__init__()
         self.all_images = []
-        #TODO
-        # تمامی عکس های گلوله را لود کنید
-        # و به این لیست اضافه نمایید
+        for i in range(5):
+            img = pygame.image.load(f"./Objects/Bullet/Bullet_00{i}.png")
+            self.all_images.append(img)
+
+        self.frame_index = 0
+        self.image = self.all_images[self.frame_index]
+        self.rect = self.image.get_rect(topleft = (x,y))
+        self.direction = direction
+        self.last_animation_time = pygame.time.get_ticks()
         
 
-    
+    def update(self):
+
+        self.image = self.all_images[self.frame_index]
+        if pygame.time.get_ticks() - self.last_animation_time >= 100:
+            self.last_animation_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            if self.frame_index >= len(self.all_images):
+                self.frame_index = 0
